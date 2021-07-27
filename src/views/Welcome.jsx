bfffac763141c305d6245bf707770a67af383297
josedavms13 <josedav0ms13@gmail.com>
2021-07-27 09:06:16 -0500

    Welcome page build -> Making token system

--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import './viewsStyles/welcomeStyles.css'
+import React, {useEffect, useState} from 'react';
 
+import './viewsStyles/welcomeStyles.css'
 import Button from "react-bootstrap/Button";
 import {InputGroup} from "react-bootstrap";
 import {FormControl} from "react-bootstrap";
 import {Form} from "react-bootstrap";
 
-function Welcome(props) {
+import { useForm } from "react-hook-form";
+
+function Welcome({props}) {
+
+    const {handleSubmit, register } = useForm();
+
+    const [continueButtonOn, SetContinueButtonDisabled] = useState(true);
+
+    const [tokenValue, SetTokenValue] = useState('');
+
+    useEffect(()=>{
+        console.log(tokenValue.length);
+        if (tokenValue) {
+            if (tokenValue.length === 20) {
+                SetContinueButtonDisabled(false)
+            } else {
+                SetContinueButtonDisabled(true);
+            }
+        }
+    },[tokenValue])
+
     return (
         <div className={'welcome'}>
 
             <h1 className={''}>Bienvenid@</h1>
+            <form action="">
 
-            <Form.Label htmlFor="basic-url">Inserta tu código</Form.Label>
-            <InputGroup className="mb-3">
+                <Form.Label htmlFor="basic-url">Inserta tu código</Form.Label>
+                <InputGroup className="mb-3">
 
-                <FormControl id="basic-url" aria-describedby="basic-addon3" />
-            </InputGroup>
+                    <FormControl id="basic-url" aria-describedby="basic-addon3" {...register('token')} onChange={(e)=>{SetTokenValue(e.target.value)}}/>
+                </InputGroup>
+            </form>
 
-            <Button variant="primary" disabled>Continuar</Button>
+            <Button variant="primary" disabled={continueButtonOn}>Continuar</Button>
         </div>
     );
 }
